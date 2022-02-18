59c5fffcdbba388845b60e7e17a516612354e153
Manan Loshali <loshalimanan@gmail.com>
2022-02-18 22:55:40 +0530

    First Commit

---
+++ images/ecm.js
@@ -0,0 +1,62 @@
+function buttonClicked() {
+    console.log("Button Clicked")
+}
+// let ulEl=document.getElementById("unordered-el")
+// let inputEl=document.getElementById("input-el")
+
+//  let myArr=[]
+//  console.log(myArr)
+// let inputBtn = document.getElementById("save-btn");
+// inputBtn.addEventListener("click",function(){
+//     console.log("Button clicked by the event listner")
+//    // myArr.push("www.awesomelead.com")
+//     myArr.push(inputEl.value)
+//     console.log(myArr)
+// })
+
+// // let box=document.getElementById("box")
+// // box.addEventListener("click",function(){
+// //     console.log("Open the box")
+// // })
+// let listItems = ""
+// for(let i=0;i<myArr.length;i++){
+// //ulEl.innerHTML+="<li>"+myArr[i]+"</li>"
+// listItems += "<li>"+myArr[i]+"</li>"
+// console.log(listItems)
+// }
+// ulEl.innerHTML = listItems
+// const butn = document.getElementById("buy")
+// butn.innerHTML = "<button> BUY </button>"
+
+// butn.addEventListener("click",function(){
+//     butn.innerHTML+="<p>Thanks for buying</p>"
+// })
+// for(let i=0;i<myArr.length;i++){
+
+// let list = document.createElement("li")
+// list.textContent= myArr[i]
+// ulEl.append(list)
+//}
+
+// const recepent = "kane"
+// const email = `Hey 
+// ${recepent} 
+// How is going on!
+// Cheers Per`
+// console.log(email)
+
+//localStorage.setItem("myName","Manan Loshali")
+// console.log(localStorage.getItem("myName"))
+// localStorage.clear()
+
+// let myStrings = `["www.loshali.com"]`
+//  myStrings = JSON.parse(myStrings)
+// myStrings.push("www.manan.com")
+// console.log(myStrings)
+// myStrings = JSON.stringify(myStrings)
+// console.log(typeof myStrings)
+
+
+  //     myItems += "<li> <a target='_blank' href='"+myLeads+"'>" + myLeads[i] + "</a></li>"
+
+  //renderLead()
